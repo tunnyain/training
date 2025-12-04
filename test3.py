1954537ae93a3bd609115d683475c94aaf65c4f1
Olatunde Aina <olatunde.aina@ceslintlgroup.com>
2025-12-04 18:01:10 +0100

    creating conflict from main

--- test3.py
+++ test3.py
@@ -1,1 +1,6 @@
-adding py file
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+year = 2025 - age
+print("Hello, " + name + "!")
+print("You were born in the year:", year)
+print("Have a great day!")
